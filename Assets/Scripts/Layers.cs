a1f7903b8b6b9c7d47236460aefae2a9b8218bea
PaddyHly <paddyhly@gmail.com>
2022-03-22 11:20:04 +0000

    tracking the hands for some reason

---
+++ Assets/Scripts/Layers.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public static class Layers
+    {
+        public const int Default = 0;
+        public const int DefaultMask = 1;
+        public const int TransparentFX = 1;
+        public const int TransparentFXMask = 2;
+        public const int IgnoreRaycast = 2;
+        public const int IgnoreRaycastMask = 4;
+        public const int Water = 4;
+        public const int WaterMask = 16;
+        public const int UI = 5;
+        public const int UIMask = 32;
+        public const int FingerTip = 6;
+        public const int FingerTipMask = 64;
+        public const int ThumbTip = 7;
+        public const int ThumbTipMask = 128;
+        public const int Palm = 8;
+        public const int PalmMask = 256;
+    }
+}
