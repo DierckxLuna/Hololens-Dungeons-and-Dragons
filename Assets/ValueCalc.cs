91b53b102b88e2600b3f40e9d3d421cd9a8a7c55
Patrick Jennings <pjennings@microsoft.com>
2022-03-25 12:37:14 +0000

    Tidy up script and log value for a d8

--- Assets/ValueCalc.cs
+++ Assets/ValueCalc.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ValueCalc : MonoBehaviour
 {
+    public bool logToConsole;
+    public int dieType;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,47 @@ public class ValueCalc : MonoBehaviour
     {
         float angleX = gameObject.transform.rotation.eulerAngles.x;
         float angleY = gameObject.transform.rotation.eulerAngles.y;
+        float angleZ = gameObject.transform.rotation.eulerAngles.z;
+
+        float angleXZ = (angleX + angleZ) % 360;
+
+        int orientation = 0;
+
+
+        switch(dieType)
+        {
+            case 6:
+                orientation = d6Val();
+                break;
+            case 8:
+                orientation = d8Val();
+                break;          
+        }
+
+        if(logToConsole){
+            Debug.Log(gameObject.name + ": " + orientation);
+        }        
+    }
+
+    bool FuzzyBounds(float angle, int toCheck)
+    {
+        int margin = 6;
+        int lowerBound = toCheck - margin;
+        int upperBound = toCheck + margin;
+
+       if (lowerBound < 0 && angle > upperBound){
+        angle = angle - 360;
+        }
+        else if (angle < lowerBound){
+        angle = angle + 360;
+        }
+        return angle >= lowerBound && angle <= upperBound;
+
+    }
+
+    int d6Val()
+    {
+        float angleX = gameObject.transform.rotation.eulerAngles.x;
         float angleZ = gameObject.transform.rotation.eulerAngles.z;
 
         float angleXZ = (angleX + angleZ) % 360;
@@ -46,35 +90,56 @@ public class ValueCalc : MonoBehaviour
             orientation = 1;
         }
 
-        Debug.Log(orientation);
-        
+        return orientation;
     }
 
-    bool FuzzyBounds(float angle, int toCheck)
+    int d8Val()
     {
-        int margin = 6;
-        int lowerBound = toCheck - margin;
-        int upperBound = toCheck + margin;
+        float angleX = gameObject.transform.rotation.eulerAngles.x;
+        float angleZ = gameObject.transform.rotation.eulerAngles.z;
+        float angleXZ = (angleX + angleZ) % 360;
 
-        if (angle >= 360){
-			angle = angle - 360;
-		}
+        int orientation = 0;
 
-        if(lowerBound >= 0 && upperBound < 360){
-            return angle >= lowerBound && angle <= upperBound;
-        }
-
-        if(lowerBound < 0){
-            if(angle > upperBound){
-                angle = angle - 360;
+        if(angleX <= 180)
+        {
+            if(FuzzyBounds(angleXZ, 80) || FuzzyBounds(angleXZ, 370))
+            {
+                orientation = 5;
             }
-            return angle >= lowerBound && angle <= upperBound;
+            else if(FuzzyBounds(angleXZ, 170) || FuzzyBounds(angleXZ, 100))
+            {
+                orientation = 3;
+            } 
+            else if(FuzzyBounds(angleXZ, 260) || FuzzyBounds(angleXZ, 190))
+            {
+                orientation = 7;
+            } 
+            else if(FuzzyBounds(angleXZ, 350) || FuzzyBounds(angleXZ, 280))
+            {
+                orientation = 8;
+            } 
+        }
+        else
+        {
+            if(FuzzyBounds(angleXZ, 80) || FuzzyBounds(angleXZ, 370))
+            {
+                orientation = 2;
+            }
+            else if(FuzzyBounds(angleXZ, 170) || FuzzyBounds(angleXZ, 100))
+            {
+                orientation = 1;
+            } 
+            else if(FuzzyBounds(angleXZ, 260) || FuzzyBounds(angleXZ, 190))
+            {
+                orientation = 4;
+            } 
+            else if(FuzzyBounds(angleXZ, 350) || FuzzyBounds(angleXZ, 280))
+            {
+                orientation = 6;
+            } 
         }
 
-        if(angle < lowerBound){
-            angle = angle + 360;
-        }
-        return angle >= lowerBound && angle <= upperBound;
-
+        return orientation;
     }
 }
